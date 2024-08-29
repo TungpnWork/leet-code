635e4df60c8f980217cf3f3ab38da41edf9c46e0
tungpnkp <tungpn@kaopiz.com>
2024-08-29 15:36:20 +0700

    rm 30 dayjs

--- 30daysJs/2634.js
+++
@@ -1,9 +0,0 @@
-var filter = function(arr, fn) {
-    newArr = [];
-    for (let i = 0; i<arr.length; i ++ ) {
-        if (fn(arr[i], i)) {
-            newArr.push(arr[i])
-        }
-    }
-    return newArr;
-};
--- 30daysJs/2635.js
+++
@@ -1,6 +0,0 @@
-var map = function(arr, fn) {
-    for(let i=0;i<arr.length;i++){
-        arr[i]=fn(arr[i],i);
-    }
-    return arr
-};
